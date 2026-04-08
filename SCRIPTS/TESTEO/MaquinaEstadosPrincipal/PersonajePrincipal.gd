8316d2423ab23b74a7bdf218a0fd1b1ee06a75d4
XogosAquiDEV <olimial03@gmail.com>
2026-04-08 20:58:43 +0200

    Añadiendo FSM y State

---
+++ SCRIPTS/TESTEO/MaquinaEstadosPrincipal/PersonajePrincipal.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+class_name prueba_goblin
+
+# Velocidad del goblin
+const WALKING_SPEED = 100
+
+# Se ejecuta al inicio del progroma
+func _ready():
+	pass
+	
+# Se ejecuta en cada frame del juego
+func _physics_process(_delta):
+	pass
